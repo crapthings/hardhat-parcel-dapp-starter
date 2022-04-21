80b603136bf9a2dc17e6a5ef39d1afae4b372f26
crapthings <crapthings@gmail.com>
2022-04-21 10:02:36 +0800

    update dialog

--- dapp-ui/src/composites/different-layout.js
+++ dapp-ui/src/composites/different-layout.js
@@ -1,5 +1,11 @@
 import { Link } from 'react-router-dom'
 
+import { Dialog } from '@headlessui/react'
+
+const localStore = createStore(() => ({
+  open: false
+}))
+
 export default function DifferentLayout () {
   const code = `export { default as layout } from '$c/blank'`
 
@@ -8,8 +14,45 @@ export default function DifferentLayout () {
       <div>different layout</div>
       <code>{code}</code>
       <Link to='/'>back</Link>
+      <button onClick={() => modalStore.setState({ open: true })}>open dialog</button>
+      <Modal />
     </div>
   )
 }
 
 export { default as layout } from '$c/blank'
+
+const modalStore = createStore(() => ({
+  open: false
+}))
+
+const close = () => modalStore.setState({ open: false })
+
+function Modal () {
+  const open = modalStore(state => state.open)
+
+  return (
+    <Dialog open={open} onClose={close} className='overflow-y-auto fixed z-50 inset-0 grid place-content-center'>
+      <Dialog.Overlay className='fixed inset-0 bg-black opacity-30' />
+
+      <div className='relative p-4 space-y-4 bg-white'>
+        <Dialog.Title>Deactivate account</Dialog.Title>
+
+        <Dialog.Description>
+          This will permanently deactivate your account
+        </Dialog.Description>
+
+        <p>
+          Are you sure you want to deactivate your account? All of your data will
+          be permanently removed. This action cannot be undone.
+        </p>
+
+        <div className='space-x-4'>
+          <button className='btn btn-sm' onClick={close}>Deactivate</button>
+          <button className='btn btn-sm' onClick={close}>Cancel</button>
+        </div>
+      </div>
+
+    </Dialog>
+  )
+}
